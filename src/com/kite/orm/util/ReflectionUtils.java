8018a92e2e7946d3535d6a39ef8e39b54bca0084
Deepak Garg <deipakgarg@gmail.com>
2016-09-28 01:31:42 +0530

    Revert "Kite 2.0"

---
+++ src/com/kite/orm/util/ReflectionUtils.java
@@ -0,0 +1,114 @@
+package com.kite.orm.util;
+
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.springframework.dao.DataAccessException;
+
+
+/**
+ * Utility Class related to Reflection Methods
+ * @author Deepak Garg
+ *
+ */
+public class ReflectionUtils
+{
+	/**
+	 * @param object
+	 * @param strFieldName
+	 * @return T value of field
+	 * @throws DataAccessException
+	 */
+	@SuppressWarnings({"serial"})
+	public static <T extends Object> T getFieldValue(T object, String strFieldName) throws DataAccessException
+	{
+// Field f;
+		try
+		{
+// f = object.getClass().getDeclaredField(strFieldName);
+// f.setAccessible(true);
+// return (T)f.get(object);
+			return getValue(object, strFieldName);
+		}
+		catch (SecurityException e)
+		{
+			e.printStackTrace();
+			throw new DataAccessException("SecurityException", e)
+				{
+				};
+		}
+		catch (IllegalArgumentException e)
+		{
+			e.printStackTrace();
+			throw new DataAccessException("IllegalArgumentException", e)
+				{
+				};
+		}
+		catch (IllegalAccessException e)
+		{
+			e.printStackTrace();
+			throw new DataAccessException("IllegalAccessException", e)
+				{
+				};
+		}
+		catch (InvocationTargetException e)
+		{
+			e.printStackTrace();
+			throw new DataAccessException("InvocationTargetException", e)
+				{
+				};
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static <T extends Object> T getValue(T object, String strFieldName) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException
+	{
+		Class<T> d = (Class<T>)object.getClass();
+		T e = object;
+		
+		
+		String[] fieldnames = strFieldName.split("\\.");
+		for (int i = 0; i < fieldnames.length; i++)
+		{
+			String name = fieldnames[i];
+			StringBuilder newName = new StringBuilder(name.length());
+			for (int k = 0; k < name.length(); k++)
+			{
+				if (k == 0)
+				{
+					newName.append(Character.toUpperCase(name.charAt(k)));
+				}
+				else
+				{
+					newName.append(name.charAt(k));
+				}
+			}
+			Method[] methods = d.getMethods();
+			Method tobeinvoked = null;
+			for (int j = 0; j < methods.length; j++)
+			{
+				if (("get" + newName.toString()).equals(methods[j].getName()))
+				{
+					tobeinvoked = methods[j];
+					break;
+				}
+			}
+			
+			if (tobeinvoked == null)
+			{
+				return null;
+			}
+			
+			e = (T)tobeinvoked.invoke(e, new Object[0]);
+			
+			if (e == null)
+			{
+				return null;
+			}
+			d = (Class<T>)e.getClass();
+			
+		}
+		return e;
+	}
+}
